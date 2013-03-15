827235e6c867d6b6468f69e43e5c3581c6d7b066
birinhos <birinhos@93810a56-b664-4e73-961e-a365e8ead931>
2013-03-15 20:45:13 +0000

    BetManager abstract class done. BetManagerSim wip

---
+++ trunk/src/bets/BetManager.java
@@ -0,0 +1,109 @@
+package bets;
+
+import java.util.Vector;
+
+import DataRepository.MarketData;
+import DataRepository.RunnersData;
+
+public abstract class  BetManager {
+	
+	// THREAD
+	
+	
+	//Market
+	public MarketData md;
+	
+	public BetManager(MarketData mdA) {
+		md=mdA;
+	}
+	
+	/**
+	 * 
+	 * @return the monitored Market  
+	 */
+	public MarketData getMd() {
+		return md;
+	}
+	
+	/**
+	 * Make polling = true; 
+	 */
+	public abstract void startPolling();
+	
+	/**
+	 * Make polling = false;
+	 */
+	public abstract void stopPolling();
+	
+	public abstract boolean  isPolling();
+	
+	/**
+	 * 
+	 * @return All bets that entered the manager by placeBet()
+	 */
+	public abstract Vector<BetData> getBets();
+	
+	
+	/**
+	 * 
+	 * @param rd - Runner 
+	 * @return All bets placed on rd
+	 */
+	public abstract Vector<BetData> getBetsByRunner(RunnersData rd);
+	
+	/**
+	 * 
+	 * @param ID - Id of bet
+	 * @return return the bet with ID
+	 */
+	public abstract BetData getBetById(long ID);
+	
+	/**
+	 * After a bet is placed the BetData fields will be altered by the manager. After placed the BetData should be read Only
+	 * 
+	 * @param bet - BetData
+	 * @return 
+	 * 0 All placed Ok
+	 * -1 Nothing placed
+	 * -2 At least some not places 
+	 */
+	public abstract int placeBet(BetData bet);
+	
+	/**
+	 * After a bet is placed the BetData fields will be altered by the manager. After placed the BetData should be read Only 
+	 * 
+	 * @param place - vector of BetData
+	 * @return 
+	 * 0 All placed Ok
+	 * -1 Nothing placed
+	 * -2 At least some not places 
+	 */
+	public abstract int placeBets(Vector<BetData> place);
+	
+	/**
+	 * 
+	 * @param bet - BetData
+	 * @return 
+	 * 0 canceled Ok
+	 * -1 Nothing placed
+	 * -2 At least some not places 
+	 */
+	public abstract int cancelBet(BetData bet);
+	
+	
+	/**
+	 * 
+	 * @param place - vector of BetData
+	 * @return 
+	 * 0 All canceled Ok
+	 * -1 Nothing placed
+	 * -2 At least some not places 
+	 */
+	public abstract int cancelBets(Vector<BetData> cancelBets);
+	
+	/**
+	 * Clean memory and make all bets became UNMONITORED
+	 */
+	public abstract void clean();
+	
+}
