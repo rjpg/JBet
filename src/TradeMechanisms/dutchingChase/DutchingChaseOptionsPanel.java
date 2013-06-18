382a2ddbffcfb95900930b77465c6de2864a93f1
birinhos <birinhos@93810a56-b664-4e73-961e-a365e8ead931>
2013-06-18 13:56:48 +0000

    duching chase bot package wip

--- src/TradeMechanisms/dutchingChase/DutchingChaseOptionsPanel.java
+++ src/TradeMechanisms/dutchingChase/DutchingChaseOptionsPanel.java
@@ -1,9 +1,39 @@
 package TradeMechanisms.dutchingChase;
 
+import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 
+import TradeMechanisms.TradeMechanism;
+import bets.BetData;
+
+import DataRepository.RunnersData;
+
 public class DutchingChaseOptionsPanel extends JPanel{
-	JPanel betCloseInfoPanel;
+	
+	//Not optional by the user
+	RunnersData rd;
+	double odd=1000;
+	double stake=2.00;
+	//--------------------------
+	
+	public JCheckBox checkIP;
+	
+
+	/*int stopLossTicks=1;
+	int waitFramesNormal=20;
+	int waitFramesBestPrice=10;
+	int updateInterval=TradeMechanism.SYNC_MARKET_DATA_UPDATE;
+	boolean forceCloseOnStopLoss=true;
+	boolean useStopProfitInBestPrice=false;
+	boolean goOnfrontInBestPrice=false;
+	int startDelay=-1;
+	int ignoreStopLossDelay=-1;*/
+	
+	
+	public DutchingChaseOptionsPanel(RunnersData rdA) {
+		rd=rdA;
+		
+	}
 	
 	
 
