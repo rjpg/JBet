0978a7ae49d0bced7fb845560a6a4aee8a5c5ea1
birinhos <birinhos@93810a56-b664-4e73-961e-a365e8ead931>
2013-08-24 22:54:12 +0000

    multiple lay horse bots, ranges in constructor

---
+++ src/bots/horseLay3Bot/HorseLayOptions.java
@@ -0,0 +1,80 @@
+package bots.horseLay3Bot;
+
+public class HorseLayOptions {
+	double entryOdd=4;
+	double aboveOdd=6;
+	
+	double numberOffRunnersLow=1;
+	double numberOffRunnersHigh=30;
+	
+	double timeHourLow=10;
+	double timeHourHigh=22;
+	
+	double lenghtInSecondsLow=50;
+	double lenghtInSecondsHigh=500;
+	
+	double liquidityLow=1000;
+	double liquidityHigh=10000000;
+
+	public double getEntryOdd() {
+		return entryOdd;
+	}
+	public void setEntryOdd(double entryOdd) {
+		this.entryOdd = entryOdd;
+	}
+	public double getAboveOdd() {
+		return aboveOdd;
+	}
+	public void setAboveOdd(double aboveOdd) {
+		this.aboveOdd = aboveOdd;
+	}
+	public double getNumberOffRunnersLow() {
+		return numberOffRunnersLow;
+	}
+	public void setNumberOffRunnersLow(double numberOffRunnersLow) {
+		this.numberOffRunnersLow = numberOffRunnersLow;
+	}
+	public double getNumberOffRunnersHigh() {
+		return numberOffRunnersHigh;
+	}
+	public void setNumberOffRunnersHigh(double numberOffRunnersHigh) {
+		this.numberOffRunnersHigh = numberOffRunnersHigh;
+	}
+	public double getTimeHourLow() {
+		return timeHourLow;
+	}
+	public void setTimeHourLow(double timeHourLow) {
+		this.timeHourLow = timeHourLow;
+	}
+	public double getTimeHourHigh() {
+		return timeHourHigh;
+	}
+	public void setTimeHourHigh(double timeHourHigh) {
+		this.timeHourHigh = timeHourHigh;
+	}
+	public double getLenghtInSecondsLow() {
+		return lenghtInSecondsLow;
+	}
+	public void setLenghtInSecondsLow(double lenghtInSecondsLow) {
+		this.lenghtInSecondsLow = lenghtInSecondsLow;
+	}
+	public double getLenghtInSecondsHigh() {
+		return lenghtInSecondsHigh;
+	}
+	public void setLenghtInSecondsHigh(double lenghtInSecondsHigh) {
+		this.lenghtInSecondsHigh = lenghtInSecondsHigh;
+	}
+	public double getLiquidityLow() {
+		return liquidityLow;
+	}
+	public void setLiquidityLow(double liquidityLow) {
+		this.liquidityLow = liquidityLow;
+	}
+	public double getLiquidityHigh() {
+		return liquidityHigh;
+	}
+	public void setLiquidityHigh(double liquidityHigh) {
+		this.liquidityHigh = liquidityHigh;
+	}
+	
+}
