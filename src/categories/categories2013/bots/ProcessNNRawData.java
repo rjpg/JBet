2ab1754478d1b22ff5bed5bf667e03b4c68ed4b0
birinhos <birinhos@93810a56-b664-4e73-961e-a365e8ead931>
2014-07-21 16:32:03 +0000

    Normalization for each category complete - TODO : convert to binary encog File.

--- src/categories/categories2013/bots/ProcessNNRawData.java
+++ src/categories/categories2013/bots/ProcessNNRawData.java
@@ -2,15 +2,17 @@ package categories.categories2013.bots;
 
 import java.awt.Color;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Vector;
 
 import javax.swing.JFrame;
 
+import DataRepository.Utils;
 import GUI.MyChart2D;
-
 import categories.categories2011.Histogram;
 import categories.categories2013.CategoryNode;
 import categories.categories2013.Root;
@@ -18,29 +20,7 @@ import demo.util.Display;
 
 public class ProcessNNRawData {
 	
-	public static int[][] toNominalTable={
-		{0,0,0}, //0
-		{0,1,1}, //1
-		{0,1,2}, //2
-		{0,1,3}, //3
-		{0,1,3}, //4
-		{0,2,4}, //5
-		{0,2,5}, //6
-		{}, //7
-		{}, //8
-		{}, //9
-		{}, //10
-		{}, //11
-		{}, //12
-		{}, //13
-		{}, //14
-		{}, //15
-		{}, //16
-		{}, //17
-		{}, //18
-		{}, //19
-		{}, //20
-		};
+
 	
 	public static BufferedReader getBufferedReader(File f)
 	{
@@ -179,7 +159,7 @@ public class ProcessNNRawData {
 			chart.addValue("interval", point+1, 0, Color.RED);
 		}
 		
-		frame.setSize(1000, 1000);
+		frame.setSize(1000, 500);
 		frame.setVisible(true);
 		
 		return frame;
@@ -187,7 +167,7 @@ public class ProcessNNRawData {
 	
 	public static double[][] findMinMax(Vector<double[]> examples)
 	{
-		double minmax[][]=new double[DataWindowsSizes.INPUT_NEURONS+1][2];
+		double minmax[][]=new double[DataWindowsSizes.INPUT_NEURONS+2][2];
 		
 		
 		for(int i=0;i<DataWindowsSizes.INPUT_NEURONS+1;i++)
@@ -206,25 +186,60 @@ public class ProcessNNRawData {
 			minmax[i][0]=histogram.getMinFiltred(filter);
 			minmax[i][1]=histogram.getMaxFiltred(filter);
 			
+			if(i==DataWindowsSizes.INPUT_NEURONS)
+			{
+				
+				/// special process to find min max for output train for symmetry in int
+				double minOutput=(int)(Math.abs(minmax[DataWindowsSizes.INPUT_NEURONS][0])+0.5);
+				if(minmax[DataWindowsSizes.INPUT_NEURONS][1]>minOutput)
+				{
+					minmax[DataWindowsSizes.INPUT_NEURONS][0]=-minOutput;
+					minmax[DataWindowsSizes.INPUT_NEURONS][1]=minOutput;
+				}
+				else
+				{
+					minmax[DataWindowsSizes.INPUT_NEURONS][0]=-(int)(minmax[DataWindowsSizes.INPUT_NEURONS][1]+0.5);
+					minmax[DataWindowsSizes.INPUT_NEURONS][1]=(int)(minmax[DataWindowsSizes.INPUT_NEURONS][1]+0.5);
+				}
+				//System.out.println("Output min max :"+minmax[DataWindowsSizes.INPUT_NEURONS][0]+" "+minmax[DataWindowsSizes.INPUT_NEURONS][1]);
+				
+				
+				minmax[i+1][0]=Math.round(histogram.getMaxFiltred(65));
+				minmax[i+1][1]=Math.round(histogram.getMaxFiltred(77));
+				
+				//System.out.println(" Max  "+minmax[i+1][0]+"[");
+				
+				
+				//System.out.println(" no move  "+minmax[i+1][0]+"[");
+				//System.out.println(" small move  "+minmax[i+1][1]+"[");
+			}
+			
 			Vector<Double> points=new Vector<Double>();
 			points.add(minmax[i][0]);
 			points.add(minmax[i][1]);
 			
 			
-			if(i==DataWindowsSizes.INPUT_NEURONS)
-			{
-				JFrame f=showHistogramChart(histogram, points,""+i);
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				f.setVisible(false);
-				f.dispose();
-				
-			}
+//			if(i==DataWindowsSizes.INPUT_NEURONS)
+//			{
+//				JFrame f=showHistogramChart(histogram, points,""+i);
+//				try {
+//					Thread.sleep(1000);
+//				} catch (InterruptedException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//				
+//				try {
+//					Display.getStringAnswer("pause - input : "+i);
+//				} catch (IOException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//				
+//				f.setVisible(false);
+//				f.dispose();
+//				
+//			}
 			
 			/*JFrame f=showHistogramChart(histogram, points,""+i);
 			
@@ -238,19 +253,6 @@ public class ProcessNNRawData {
 			f.dispose();*/
 		}
 		
-		/// special process to find min max for output train for symmetry in int
-		double minOutput=(int)(Math.abs(minmax[DataWindowsSizes.INPUT_NEURONS][0])+0.5);
-		if(minmax[DataWindowsSizes.INPUT_NEURONS][1]>minOutput)
-		{
-			minmax[DataWindowsSizes.INPUT_NEURONS][0]=-minOutput;
-			minmax[DataWindowsSizes.INPUT_NEURONS][1]=minOutput;
-		}
-		else
-		{
-			minmax[DataWindowsSizes.INPUT_NEURONS][0]=-(int)(minmax[DataWindowsSizes.INPUT_NEURONS][1]+0.5);
-			minmax[DataWindowsSizes.INPUT_NEURONS][1]=(int)(minmax[DataWindowsSizes.INPUT_NEURONS][1]+0.5);
-		}
-		System.out.println("Output min max :"+minmax[DataWindowsSizes.INPUT_NEURONS][0]+" "+minmax[DataWindowsSizes.INPUT_NEURONS][1]);
 		
 		//System.out.println(""+((int)4.9));
 		
@@ -275,15 +277,174 @@ public class ProcessNNRawData {
 		}
 	}
 	
-	public static void wtriteMinMaxValues(double[][] minmax,Vector<CategoryNode> cat)
+	public static void writeMinMaxValues(double[][] minmax,Vector<CategoryNode> cat)
 	{
 		String fileName=CategoryNode.getAncestorsStringPath(cat)+"NNMinMax.csv";
-		for(int i=0;i<DataWindowsSizes.INPUT_NEURONS+1;i++)
+		
+		deleteFile(fileName);
+		
+		String s="";
+		for(int i=0;i<DataWindowsSizes.INPUT_NEURONS+2;i++)
 		{
-			
+			s+=minmax[i][0]+" "+minmax[i][1]+"\n";
 		}
+		
+		BufferedWriter out=null;
+		
+		try {
+			out = new BufferedWriter(new FileWriter(fileName, true));
+			} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Error Open "+fileName+" for writing");
+			}
+		if(out==null)
+		{
+			System.err.println("could not open "+fileName);
+			return;
+		}
+		
+		try {
+			out.write(s);
+			out.newLine();
+			out.flush();
+		} catch (IOException e) {
+			System.out.println(fileName+":Error wrtting data to log file");
+			e.printStackTrace();
+		}
+		
+		try {
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
+	public static Vector<double[]> normalize(Vector<double[]> examples,double[][] minmax)
+	{
+		Vector<double[]> ret=new Vector<double[]>();
+		
+		for(double[] rawExample:examples)
+		{
+			double normalExample[]=new double[DataWindowsSizes.INPUT_NEURONS+1];
+		
+			for(int i=0;i<DataWindowsSizes.INPUT_NEURONS+1;i++)
+			{	
+				if(i==DataWindowsSizes.INPUT_NEURONS)
+				{
+					
+					normalExample[i]=normalizeOutput(rawExample[i],minmax[i+1][0],minmax[i+1][1]);
+					//System.out.println("Normalize["+i+"]="+rawExample[i]+" with ("+minmax[i+1][0]+","+minmax[i+1][1]+") = "+normalExample[i]);
+				}
+				else
+				{
+					
+					normalExample[i]=normalizeValue(rawExample[i], minmax[i][0], minmax[i][1]);
+					//System.out.println("Normalize["+i+"]="+rawExample[i]+" with ("+minmax[i][0]+","+minmax[i][1]+") = "+normalExample[i]);
+				}
+				
+				//try {
+				//	Display.getStringAnswer("pause - input : "+i);
+				//} catch (IOException e) {
+				//	// TODO Auto-generated catch block
+				//	e.printStackTrace();
+				//}
+			}
+			ret.add(normalExample);
+		}
+		
+		return ret;
+	}
+	
+	public static double normalizeValue(double value,double min,double max)
+	{
+		double ret=0;
+		
+		double total=max-min;
+		double relative=value-min;
+		
+		ret=(relative*2)/total;
+				
+		ret-=1;	
+		
+		if(ret>1) ret=1.0;
+		
+		if(ret<-1) ret=-1.0;
+		
+		return ret;
+	}
+
+	public static double normalizeOutput(double value,double min,double max)
+	{
+		double ret=0;
+		if(value>-min && value<min)
+			ret=0.0;
+		else if(value>=min && value<=max)
+			ret=0.5;
+		else if(value>max)
+			ret=1.0;
+		else if(value>=-max && value<=-min)
+			ret=-0.5;
+		else if(value<-max)
+			ret=-1.0;
+	
+		return ret;
+	}
+	
+	public static void writeTalbleFile(Vector<double[]> examples,Vector<CategoryNode> cat,String fileNameA)
+	{
+		String fileName=CategoryNode.getAncestorsStringPath(cat)+fileNameA;
+		
+		System.out.println("Writting data to file - "+fileName);
+		
+		deleteFile(fileName);
+		
+		BufferedWriter out=null;
+		
+		try {
+			out = new BufferedWriter(new FileWriter(fileName, true));
+			} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Error Open "+fileName+" for writing");
+			}
+		if(out==null)
+		{
+			System.err.println("could not open "+fileName);
+			return;
+		}
+		
+		
+		for(double[] ex:examples)
+		{
+			String lineExample="";
+			for(double value:ex)
+			{
+				lineExample+=value+" ";
+			}
+			//lineExample+="\n";
+			
+			
+			try {
+				out.write(lineExample);
+				out.newLine();
+				out.flush();
+			} catch (IOException e) {
+				System.out.println("Error wrtting data to file - "+fileName);
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println("Writting data to file over and OK - closing - "+fileName);
+		
+		try {
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
 			
 			Root root=new Root(0);
@@ -291,8 +452,8 @@ public class ProcessNNRawData {
 			CategoryNode.printIDs(root);
 			//CategoryNode.buildDirectories(root);
 			
-			//int i=203;
-			for(int i=0;i<648;i++)
+			int i=203;
+			//for(int i=0;i<648;i++)
 			{
 				Vector<CategoryNode> cat=CategoryNode.getAncestorsById(root,i);
 				String fileName=CategoryNode.getAncestorsStringPath(cat)+"NNRawData.csv";
@@ -311,9 +472,16 @@ public class ProcessNNRawData {
 					
 					double minmax[][]=findMinMax(collectExamples);
 					
-					wtriteMinMaxValues(minmax,cat);
+					writeMinMaxValues(minmax,cat);
+					
+					Vector<double[]> normalizeExamples=normalize(collectExamples,minmax);
+					System.out.println("Normalized - Number of examples : "+normalizeExamples.size() );
+					
+					writeTalbleFile(normalizeExamples,cat,"NNNormalizeData.csv");
 					
 					System.gc();
+					
+					
 				}
 				else
 				{
@@ -322,7 +490,8 @@ public class ProcessNNRawData {
 				
 			}
 			
-			
+			//System.out.println(normalizeValue(-89, -100, 100));
+			//System.out.println(normalizeOutput(-2.000000000000341, -55.12600000000009, 65.21000000000004));
 		}
 
 }
